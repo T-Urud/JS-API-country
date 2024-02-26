eaf59ed937c8820151c79efe09ad6114bfb04440
urud.theo@gmail.com <urud.theo@gmail.com>
2024-02-26 18:19:54 +0100

    js functions fetch and display done and ok, need to do css on card

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const countriesContainer = document.querySelector(".countries-container");
 const search = document.getElementById("inputSearch");
-console.log(search);
 let countries = [];
 
 async function fetchCountries() {
   await fetch("https://restcountries.com/v3.1/all").then((res) =>
     res.json().then((data) => (countries = data))
   );
-  console.log(countries[1]);
+  console.log(countries);
+  countriesDisplay();
+}
+
+function countriesDisplay() {
+  countriesContainer.innerHTML = countries
+    .map((country) => {
+      return `
+        <article class="countryCard">
+          <img src="${country.flags.png}" alt="${country.flags.alt}"
+          <h3>${country.name.official}</h3>
+          <p>${country.capital}</p>
+          <p>${country.population}</p>
+        </article>
+      `;
+    })
+    .join("");
 }
 fetchCountries();
 
-function countriesDisplay() {
-  countriesContainer.innerHTML = countries.map((country) => {
-    `
-      <svg>${country.flag.svg}</svg>
-      <h2>${country.name.official}</h2>
-      <p>${country.capital}</p>
-      <p>${country.population}</p>
-
-    `;
-  });
-}
-
-search.addEventListener("submit", (e) => {
-  e.preventDefault();
-  countriesDisplay();
-});
+// search.addEventListener("input", (e) => {
+//   fetchCountries(e.target.value);
+//   console.log(e.target.value);
+// });
 
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
